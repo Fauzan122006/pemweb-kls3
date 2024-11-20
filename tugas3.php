9b4c34060fb12ba7c0b500d7ed012fb8a42a07a7
Fauzan <fauzantakibang@gmail.com>
2024-11-20 16:19:27 +0800

    ubah sedikit warna + penjualan makanan

--- tugas3.php
+++ tugas3.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas</title>
+    <title>Penjualan makanan</title>
     <style>
          body {
             font-family: Arial, sans-serif;
@@ -100,7 +100,7 @@
     $host = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "penjualan_ayam";
+    $dbname = "penjualan_makanan";
 
     $conn = new mysqli($host, $username, $password, $dbname);
 
@@ -109,18 +109,18 @@
     }
 
     if (isset($_POST['kirim'])) {
-        $jenis_ayam = $_POST['jenis_ayam'];
-        $harga_ayam = $_POST['harga_ayam'];
-        $jumlah_ayam = (int)$_POST['jumlah_ayam'];
+        $jenis_makanan = $_POST['jenis_makanan'];
+        $harga_makanan = $_POST['harga_makanan'];
+        $jumlah_makanan = (int)$_POST['jumlah_makanan'];
         $diskon_persen = $_POST['diskon'];
 
-        $total_harga = $harga_ayam * $jumlah_ayam;
+        $total_harga = $harga_makanan * $jumlah_makanan;
         $diskon = ($diskon_persen / 100) * $total_harga;
         $total_setelah_diskon = $total_harga - $diskon;
 
-        $stmt = $conn ->prepare("INSERT INTO penjualan(jenis_ayam, harga_ayam, jumlah_ayam, diskon, total_harga, total_setelah_diskon) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt = $conn ->prepare("INSERT INTO penjualan(jenis_makanan, harga_makanan, jumlah_makanan, diskon, total_harga, total_setelah_diskon) VALUES (?, ?, ?, ?, ?, ?)");
 
-        $stmt ->bind_param("sdiddd", $jenis_ayam, $harga_ayam, $jumlah_ayam, $diskon_persen, $total_harga, $total_setelah_diskon);
+        $stmt ->bind_param("sdiddd", $jenis_makanan, $harga_makanan, $jumlah_makanan, $diskon_persen, $total_harga, $total_setelah_diskon);
 
         $stmt -> execute();
         $stmt -> close();
@@ -139,19 +139,19 @@
     ?>
 <body>
 <div class="container">
-        <h1>Form perhitungan penjualan ayam</h1>
+        <h1>Form perhitungan penjualan makanan</h1>
         <form method="POST" enctype='multipart/form-data'>
-            <label for="jenis_ayam">jenis ayam</label>
-            <select name="jenis_ayam" id="jenis_ayam" required>
+            <label for="jenis_makanan">jenis makanan</label>
+            <select name="jenis_makanan" id="jenis_makanan" required>
                 <option value="">Silahkan Pilih!</option>
-                <option value="ayam kampung">ayam kampung</option>
-                <option value="ayam broyler">ayam broyler</option>
-                <option value="ayam petelur">ayam petelur</option>
+                <option value="nasi goreng seafood">nasi goreng seafood</option>
+                <option value="gado gado">gado gado</option>
+                <option value="bakso">bakso</option>
             </select>
-            <label for="harga_ayam">harga satuan ayam (Rp)</label>
-            <input type="number" id="harga_ayam" name="harga_ayam" required>
-            <label for="jumlah_ayam">jumlah ayam yang dibeli</label>
-            <input type="number" id="jumlah_ayam" name="jumlah_ayam" required>
+            <label for="harga_makanan">harga satuan makanan (Rp)</label>
+            <input type="number" id="harga_makanan" name="harga_makanan" required>
+            <label for="jumlah_makanan">jumlah makanan yang dibeli</label>
+            <input type="number" id="jumlah_makanan" name="jumlah_makanan" required>
             <label for="diskon">Diskon(%)</label>
             <input type="number" id="diskon" name="diskon" min="0" max="100" required>
             <input type="submit" name="kirim" value="kirim">
@@ -162,9 +162,9 @@
             <thead>
                 <tr>
                     <th>No</th>
-                    <th>Jenis Ayam</th>
-                    <th>Harga Satuan Ayam (RP)</th>
-                    <th>Jumlah Ayam</th>
+                    <th>Jenis makanan</th>
+                    <th>Harga Satuan makanan (RP)</th>
+                    <th>Jumlah makanan</th>
                     <th>Total Harga Sebelum Diskon</th>
                     <th>Diskon (%)</th>
                     <th>Total Harga Setelah Diskon</th>
@@ -177,9 +177,9 @@
                     <?php foreach ($penjualan as $row) : ?>
                         <tr>
                             <td><?= $no++; ?></td>
-                            <td><?= $row['jenis_ayam']; ?></td>
-                            <td><?= number_format($row['harga_ayam'], 2); ?></td>
-                            <td><?= $row['jumlah_ayam'];?></td>
+                            <td><?= $row['jenis_makanan']; ?></td>
+                            <td><?= number_format($row['harga_makanan'], 2); ?></td>
+                            <td><?= $row['jumlah_makanan'];?></td>
                             <td><?= number_format($row['total_harga'], 2); ?></td>
                             <td><?= $row['diskon'];?></td>
                             <td><?= number_format($row['total_setelah_diskon'], 2); ?></td>
@@ -196,25 +196,25 @@
         <!-- <?php
         // if (isset($_POST['kirim'])) {
             // Sanitasi dan validasi input
-            // $kategori_ayam = htmlspecialchars($_POST['kategori_ayam']);
-            // $harga_ayam = $_POST['harga_ayam'];
-            // $jumlah_ayam_yang_dibeli = $_POST['jumlah_ayam_yang_dibeli'];
+            // $kategori_makanan = htmlspecialchars($_POST['kategori_makanan']);
+            // $harga_makanan = $_POST['harga_makanan'];
+            // $jumlah_makanan_yang_dibeli = $_POST['jumlah_makanan_yang_dibeli'];
             // $diskon = $_POST['diskon'];
 
-            // if ($harga_ayam === false || $jumlah_ayam_yang_dibeli === false || $diskon === false) {
+            // if ($harga_makanan === false || $jumlah_makanan_yang_dibeli === false || $diskon === false) {
             //     echo "<div class='result'>Input tidak valid.</div>";
             // } else {
                 // Perhitungan
-                // $total_harga = $jumlah_ayam_yang_dibeli * $harga_ayam;
+                // $total_harga = $jumlah_makanan_yang_dibeli * $harga_makanan;
                 // $total_diskon = $diskon / 100;
                 // $harga_diskon = $total_harga * $total_diskon;
                 // $total_bayar = $total_harga - $harga_diskon;
 
                 // Output hasil
                 // echo "<div class='result'>";
-                // echo "<p><strong>Jenis ayam:</strong> $jenis_ayam</p>";
-                // echo "<p><strong>Harga satuan ayam:</strong> Rp " . number_format($harga_ayam) . "</p>";
-                // echo "<p><strong>Jumlah ayam yang dibeli:</strong>" . number_format($jumlah_ayam) . "</p>";
+                // echo "<p><strong>Jenis makanan:</strong> $jenis_makanan</p>";
+                // echo "<p><strong>Harga satuan makanan:</strong> Rp " . number_format($harga_makanan) . "</p>";
+                // echo "<p><strong>Jumlah makanan yang dibeli:</strong>" . number_format($jumlah_makanan) . "</p>";
                 // echo "<p><strong>Total harga sebelum Diskon:</strong> Rp" . number_format($total_harga) . "</p>";
                 // echo "<p><strong>Diskon:</strong> $diskon%</p>";
                 // echo "<p><strong>Jumlah diskon:</strong> Rp" . number_format($harga_diskon) . "</p>";
